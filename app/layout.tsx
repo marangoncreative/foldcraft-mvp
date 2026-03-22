86323198309efb4eb06ef260ffdd24827c061406
marangoncreative <marangoncreative@yahoo.com>
2026-03-22 23:00:01 +0300

    Update children type to ReactNode in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react'
+
 export const metadata = {
   title: 'Foldcraft',
   description: 'Foldcraft MVP',
@@ -6,7 +8,7 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
     <html lang="en">
